2d64beb8e1e41a94476c7db697b68380f030748d
hugopascoal2570 <hugo_pascoal_@hotmail.com>
2023-10-25 11:16:25 -0300

    first commit

---
+++ routes/api.php
@@ -0,0 +1,13 @@
+<?php 
+use App\Http\Controllers\Api\Auth\AuthController;
+use App\Http\Controllers\Api\ReplySupportApiController;
+use App\Http\Controllers\Api\SupportController;
+use Illuminate\Support\Facades\Route;
+
+Route::post('/login', [AuthController::class, 'auth']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+Route::get('/me', [AuthController::class, 'me'])->middleware('auth:sanctum');
+
+Route::middleware(['auth:sanctum'])->group(function () {
+
+});
